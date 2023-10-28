6bbad2693a7ca68ce57f1a4982379e560c9e73ac
DesDesDesHMT <82308907+HMT2002@users.noreply.github.com>
2023-10-28 16:57:30 +0700

    streaming, select movie screen

--- src/screen/SingleFilm.js
+++ src/screen/SingleFilm.js
@@ -1,9 +1,16 @@
-import {React,useContext,useRef,useEffect,useState} from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
-import { Avatar } from 'react-native-elements';
+import {React, useContext, useRef, useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import {Avatar} from 'react-native-elements';
 import {GetNoteAction} from '../actions/GetNote';
 import Video from 'react-native-video';
-import WDHT from './test.mp4'
+import WDHT from './test.mp4';
 // import WDHT from './World Domination How-To.m3u8'
 import AppController from '../controllers/AppController';
 import AppContext from '../utils/AppContext';
@@ -11,78 +18,111 @@ import Clipboard from '@react-native-clipboard/clipboard';
 import {useIsFocused} from '@react-navigation/native';
 import videojs from 'video.js';
 import Hls from 'hls.js';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-const MovieDetailScreen = ({ route, navigation }) => {
-  const { movie } = route.params;
+import axios from 'axios';
+
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+
+const getHlsUrl = async filename => {
+  console.log(filename);
+  var url = 'http://192.168.1.99:9000/redirect/hls/' + filename;
+
+  const {data} = await axios({
+    method: 'get',
+    url: url,
+    headers: {myaxiosfetch: '123'},
+  });
+  console.log(data);
+  var url = data.subserverurl;
+  return url;
+};
+const getDashUrl = async filename => {
+  var url =
+    'http://192.168.1.99:9000/redirect/dash/' + filename + '/' + filename;
+
+  const {data} = await axios({
+    method: 'get',
+    url: url,
+    headers: {myaxiosfetch: '123'},
+  });
+  console.log(data);
+  var url = data.subserverurl;
+  return url;
+};
+
+const MovieDetailScreen = ({route, navigation}) => {
+  const {movie} = route.params;
   const isFocus = useIsFocused();
   const appContext = useContext(AppContext);
   const videoRef = useRef();
+  const [videoURL, setVideoURL] = useState('');
+  const [videoDASHURL, setVideoDASHURL] = useState({uri: ''});
+  const [videoHLSURL, setVideoHLSURL] = useState({uri: ''});
 
- 
   const [showVideo, setShowVideo] = useState(false);
   useEffect(() => {
-    const CheckVideoAndEncode = async () => {
-    };
+    const CheckVideoAndEncode = async () => {};
     const LoadVideo = async () => {
       try {
-        var obj_play;
-        let url = 'http://192.168.1.99:9000/redirect/hls/World Domination How-To';
+        // var obj_play;
+        // let url = 'http://192.168.1.99:9000/redirect/hls/World Domination How-To';
 
-        console.log(videoRef)
-        const config = {
-          startPosition: 0, // can be any number you want
-        };
-        obj_play = {
-          fill: true,
-          fluid: true,
-          autoplay: true,
-          controls: true,
-          preload: 'auto',
-          loop: true,
-          sources: [
-            // {
-            //   src: data.path,
-            //   type: 'application/x-mpegURL',
-            //   withCredentials: true,
-            // },
-          ],
-        };
-        const hls = new Hls(config);
-        hls.loadSource(url);
-        hls.attachMedia(videoRef.current);
-        hls.subtitleDisplay = true;
+        // console.log(videoRef)
+        // const config = {
+        //   startPosition: 0, // can be any number you want
+        // };
+        // obj_play = {
+        //   fill: true,
+        //   fluid: true,
+        //   autoplay: true,
+        //   controls: true,
+        //   preload: 'auto',
+        //   loop: true,
+        //   sources: [
+        //     // {
+        //     //   src: data.path,
+        //     //   type: 'application/x-mpegURL',
+        //     //   withCredentials: true,
+        //     // },
+        //   ],
+        // };
+        // const hls = new Hls(config);
+        // hls.loadSource(url);
+        // hls.attachMedia(videoRef.current);
+        // hls.subtitleDisplay = true;
 
-        const _player = videojs(
-          videoRef.current,
-          obj_play,
-          function onPlayerReady() {
-            videojs.log('Your player is ready!');
+        // const _player = videojs(
+        //   videoRef.current,
+        //   obj_play,
+        //   function onPlayerReady() {
+        //     videojs.log('Your player is ready!');
 
-            // In this context, `this` is the player that was created by Video.js.
-            this.play();
+        //     // In this context, `this` is the player that was created by Video.js.
+        //     this.play();
 
-            // volume scale 0 - 1
-            const defaultVolume = 0.4;
-            this.volume(defaultVolume);
+        //     // volume scale 0 - 1
+        //     const defaultVolume = 0.4;
+        //     this.volume(defaultVolume);
 
-            // How about an event listener?
-            this.on('ended', function () {
-              videojs.log('Awww...over so soon?!');
-            });
-          },
-        );
-        console.log(_player);
+        //     // How about an event listener?
+        //     this.on('ended', function () {
+        //       videojs.log('Awww...over so soon?!');
+        //     });
+        //   },
+        // );
+        // console.log(_player);
 
-        // _player.on('xhr-hooks-ready', () => {
-        //   const playerRequestHook = (options) => {
-        //     options.beforeSend = (xhr) => {
-        //       xhr.setRequestHeader('foo', 'bar');
-        //     };
-        //     console.log(options)
-        //     return options;
-        //   };
-        //   _player.tech().vhs.xhr.onResponse(playerRequestHook);
-        // });
+        var urlDash = await getDashUrl('sF06Unx');
+        setVideoDASHURL(() => {
+          return {uri: urlDash};
+        });
+
+        var urlHls = await getHlsUrl('eHQcQgU');
+        setVideoHLSURL(() => {
+          return {uri: urlHls};
+        });
+
+        console.log(urlHls);
+        console.log(urlDash);
       } catch (error) {
         console.log(error);
       }
@@ -92,65 +132,82 @@ const MovieDetailScreen = ({ route, navigation }) => {
   }, []);
   const handleGoBack = () => {
     navigation.goBack();
-  };  
-  const handlePlay = () => {
+  };
+  const handlePlay = async () => {
+    var urlDash = await getDashUrl('sF06Unx');
+    setVideoDASHURL(() => {
+      return urlDash;
+    });
+
+    var urlHls = await getHlsUrl('eHQcQgU');
+    setVideoHLSURL(() => {
+      return urlHls;
+    });
+
+    console.log(urlHls);
+    console.log(urlDash);
     setShowVideo(true);
-  };  
+  };
   return (
     <ScrollView style={styles.container}>
-       <TouchableOpacity style={{width:"20%"}} onPress={handleGoBack}>
+      <TouchableOpacity style={{width: '20%'}} onPress={handleGoBack}>
         <Text style={styles.buttonText}> Back</Text>
       </TouchableOpacity>
       {showVideo ? (
-      <Video
-      //HOW THE FUCK???? TẠI SAO HLS NGƯỜI KHÁC COI ĐC CÒN CỦA T THÌ ÉO?????
-      source={WDHT} // the video file
-      // source={{uri: "https://tzvodacomcontent.s3.amazonaws.com/video-1654952965085/video-1654952965085.m3u8"}}
-      // source={{uri: "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8"}}
+        <Video
+          source={{uri: videoHLSURL}}
+          // source={{uri: 'http://192.168.1.99:9200/videos/sF06UnxDash/init.mpd'}}
+          // source={{uri: "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8"}}
 
-      paused={false} // make it start    r
-      style={styles.image} // any style you want
-      repeat={true} // make it a loop
-      ref={videoRef} // Store reference
-      onBuffer={this.onBuffer} // Callback when remote video is buffering
-      onError={error => {
-        console.log(error);
-      }}
-    />
-    ) : (
- 
-       <Image
-        source={movie.poster}
-        style={styles.image}
-        resizeMode="cover"
-      />
-       
-    
-    )}
+          paused={false} // make it start    r
+          style={styles.image} // any style you want
+          repeat={true} // make it a loop
+          ref={videoRef} // Store reference
+          onBuffer={this.onBuffer} // Callback when remote video is buffering
+          onError={error => {
+            console.log(error);
+          }}
+        />
+      ) : (
+        <Image source={movie.poster} style={styles.image} resizeMode="cover" />
+      )}
       <Text style={styles.episodes}>Episodes: 10</Text>
       <Text style={styles.ageRestriction}>Age Restriction: 18+</Text>
       <Text style={styles.numMovies}>Number of Movies: 5</Text>
-   
+
       <TouchableOpacity style={styles.button} onPress={handlePlay}>
         <Text style={styles.buttonText}>Play Video</Text>
       </TouchableOpacity>
       <Text style={styles.description}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nunc eget nunc consectetur tincidunt. Nulla facilisi. Sed euismod, nisl ac tincidunt tincidunt, mi mauris aliquet odio, vitae aliquam nunc nunc id nunc. Sed vitae nunc eget nunc consectetur tincidunt. Nulla facilisi. Sed euismod, nisl ac tincidunt tincidunt, mi mauris aliquet odio, vitae aliquam nunc nunc id nunc.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nunc
+        eget nunc consectetur tincidunt. Nulla facilisi. Sed euismod, nisl ac
+        tincidunt tincidunt, mi mauris aliquet odio, vitae aliquam nunc nunc id
+        nunc. Sed vitae nunc eget nunc consectetur tincidunt. Nulla facilisi.
+        Sed euismod, nisl ac tincidunt tincidunt, mi mauris aliquet odio, vitae
+        aliquam nunc nunc id nunc.
       </Text>
       <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={() => console.log('Add to Playlist')}>
-            <Text style={styles.buttonText}>Add to Playlist</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => console.log('Rate Movie')}>
-            <Text style={styles.buttonText}>Rate Movie</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => console.log('Share')}>
-            <Text style={styles.buttonText}>Share</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => console.log('Download')}>
-            <Text style={styles.buttonText}>Download</Text>
-          </TouchableOpacity>
-        </View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => console.log('Add to Playlist')}>
+          <Text style={styles.buttonText}>Add to Playlist</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => console.log('Rate Movie')}>
+          <Text style={styles.buttonText}>Rate Movie</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => console.log('Share')}>
+          <Text style={styles.buttonText}>Share</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => console.log('Download')}>
+          <Text style={styles.buttonText}>Download</Text>
+        </TouchableOpacity>
+      </View>
 
       <View style={styles.commentsContainer}>
         <Text style={styles.commentsTitle}>Comments</Text>
@@ -277,14 +334,16 @@ const styles = StyleSheet.create({
   commentText: {
     fontSize: 16,
     color: '#fff',
-  }, description: {
+  },
+  description: {
     fontSize: 16,
     color: '#fff',
   },
   buttonContainer: {
     flexDirection: 'row',
     marginBottom: 8,
-  },backgroundVideo: {
+  },
+  backgroundVideo: {
     position: 'absolute',
     top: 50,
     left: 0,
